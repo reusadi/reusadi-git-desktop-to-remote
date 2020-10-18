38bf6f89a3c4928920e6e47f53bfb44cb6f15235
reusadi <ireusadi@gmail.com>
2020-10-18 13:42:00 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus quia quibusdam consequuntur non veritatis, harum numquam nihil modi porro omnis!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
